b64edc16a76af2745a28df0ff072d974709d23ad
OptiCodeSr <148829872+OptiCodeSr@users.noreply.github.com>
2024-11-19 15:38:46 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="https://github.com/atcodersairam/"
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        https://github.com/atcodersairam/
+                    </a>
+
+                    <p>
+                        Built by | &nbsp;
+                        <a
+                            href=""
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                           Sairam B N
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
